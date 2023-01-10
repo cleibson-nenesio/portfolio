945df3aba6fc91588a04c7603f6dcc81d8a914e6
Cleibson Nenesio <cleibson.nenesio@hotmail.com>
2023-01-10 13:54:16 -0300

    Added animation on scroll

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,47 @@
+const projectsList = document.querySelector('.projects-list')
+
+const technologies = {
+    react: '<i class="fab fa-react"></i>',
+    javascript: '<i class="fab fa-js"></i>',
+}
+
+const projects = [
+    {
+        name: 'Password Generator',
+        image: 'password-generator',
+        stack: [technologies.javascript]
+    },
+    {
+        name: 'Pokedéx',
+        image: 'pokedex',
+        stack: [technologies.react]
+    },
+    {
+        name: 'Memory game',
+        image: 'memory-game',
+        stack: [technologies.javascript]
+    },
+    {
+        name: 'Where in the World',
+        image: 'where-in-the-world',
+        stack: [technologies.react]
+    },
+]
+
+projects.forEach(project => {
+    projectsList.innerHTML += `
+        <div class="project ${project.image}")">
+            <h3>${project.name}</h3>
+            <div class="languages-and-frameworks">
+                ${project.stack.map(stack => innerHTML = stack)}
+            </div>
+            <div class="dark-background"></div>
+        </div>
+    `
+})
+
+//
+
 const myName = document.querySelector('.my-name')
 const subtitle = document.querySelector('h3')
 
@@ -13,3 +57,20 @@ const typeWriter = (elemen) => {
 
 typeWriter(myName)
 typeWriter(subtitle)
+
+//
+
+const elementsToAnimate = document.querySelectorAll('[data-anime]')
+
+const animateElements = (elemen) => {
+    const intersectionObserver = new IntersectionObserver(entry => {
+        if(entry[0].isIntersecting) elemen.classList.add('animate')
+        if(!entry[0].isIntersecting) elemen.classList.remove('animate')
+    })
+
+    intersectionObserver.observe(elemen)
+}
+
+elementsToAnimate.forEach(elemen => {
+    animateElements(elemen)
+})
